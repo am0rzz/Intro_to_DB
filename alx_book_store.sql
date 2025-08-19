4f90a2e3b25a244d6443314c17a6a12a3c022d51
am0rzz <punish3r297@gmail.com>
2025-08-19 10:25:14 +0300

    .

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -36,7 +36,7 @@ CREATE TABLE Order_Details(
 orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
 order_id INT,
 book_id INT,
-quantity double,
+quantity DOUBLE,
 FOREIGN KEY(order_id) REFERENCES Orders(order_id),
 FOREIGN KEY(book_id) REFERENCES Books(book_id)
 );
